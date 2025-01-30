173e527f7010dd79946c7a3e9eb872e633216f65
unknown <gaviriayeferson2@gmail.com>
2025-01-30 14:33:43 +0100

    Add getDias method to return days

--- src/main/java/dev/yeferson/lista_dias_semana/GestorDiasSemana.java
+++ src/main/java/dev/yeferson/lista_dias_semana/GestorDiasSemana.java
@@ -1,5 +1,18 @@
 package dev.yeferson.lista_dias_semana;
 
-public class GestorDiasSemana {
+import java.util.List;
 
-}
+public class GestorDiasSemana {
+    private List<String> dias;
+
+    public GestorDiasSemana(List<String> dias) {
+        this.dias = dias;
+    }
+        
+        public List<String> getDias() {
+            return dias;
+        }
+    }
+
+    
+
--- src/test/java/dev/yeferson/lista_dias_semana/GestorDiasSemanaTest.java
+++ src/test/java/dev/yeferson/lista_dias_semana/GestorDiasSemanaTest.java
@@ -4,14 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.ArrayList;
 
 public class GestorDiasSemanaTest {
     @Test
     @DisplayName("Verificar instanciación de GestorDiasSemana")
 
     void testInstanciacionGestorDiasSemana() {
-        GestorDiasSemana gestorDiasSemana = new GestorDiasSemana();
+        GestorDiasSemana gestorDiasSemana = new GestorDiasSemana(new ArrayList<>());
         assertThat(gestorDiasSemana, notNullValue());
         assertThat(gestorDiasSemana, instanceOf(GestorDiasSemana.class));
     }
@@ -19,8 +23,8 @@ public class GestorDiasSemanaTest {
     @DisplayName("Verificar que la lista dias inicia vacía")
 
     void testListaDiasVacia(){
-        GestorDiasSemana gestorDiasSemana = new GestorDiasSemana();
-        assertThat(gestorDiasSemana.getDias(), notNullValue());
+        GestorDiasSemana gestorDiasSemana = new GestorDiasSemana(new ArrayList<>());
+        assertThat(gestorDiasSemana.getDias().isEmpty() , is(true));
 
     }
 }
